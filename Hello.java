de4aafbe3ec48b2e7706651cf6a2825b9aa4922c
nsbraj@yahoo.com <nsbraj@yahoo.com>
2023-02-15 05:41:58 +0000

    println spelling changed

--- Hello.java
+++ Hello.java
@@ -3,6 +3,6 @@ class Hello
 public static void main(String args[])
 {
 System.out.println("Testing Jenkin for SDET Training");
-System.out.prinln("Today is the last session about Jenkin");
+System.out.println("Today is the last session about Jenkin");
 }
 }
